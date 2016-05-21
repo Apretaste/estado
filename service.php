8efdfd3271d67c95f3250e855d478ec85364993b
salvipascual <salvi.pascual@gmail.com>
2016-05-20 22:23:48 -0400

    fix little issues

--- service.php
+++ service.php
@@ -8,7 +8,6 @@
  */
 class Estado extends Service
 {
-
 	/**
 	 * Main behavior
 	 *
@@ -19,7 +18,9 @@ class Estado extends Service
 	 */
 	public function _main(Request $request)
 	{
-		return new Response();
+		$response = new Response();
+		$response->createFromText('Este es un servicio de administracin y debe ser usado con parametros extras. Si no conoce los parametros, pregunte a su administrador.');
+		return $response;		
 	}
 
 	/**
@@ -32,33 +33,33 @@ class Estado extends Service
 	 */
 	public function _promotor($request)
 	{
-
 		// clear query string
 		$source = trim(strtolower($request->query));
-		
+
 		// empty query/email address
-		if (trim($source) == '') {
+		if (trim($source) == '')
+		{
 			$response = new Response();
-			$response->setResponseSubject('No escribiste la direccion email que promocionas');
-			$response->createFromText('Especifique la direcci&oacute;n email que promocionas despu&eacute;s de la frase <b>ESTADO PROMOTOR</b> en el asunto del correo.');
+			$response->createFromText('Especifique el email desde el cual promocionas despu&eacute;s de la frase <b>ESTADO PROMOTOR</b> en el asunto.');
 			return $response;
 		}
-		
+
 		// connect to db
 		$db = new Connection();
-		
+
 		// get stats from db
 		$sql = "SELECT 'TOTAL' as stat, count(*) as val FROM first_timers WHERE source = '$source' UNION SELECT 'PAID' as stat, count(*) as val FROM first_timers WHERE paid = 1 AND source = '$source';";		
 		$r = $db->deepQuery($sql);
-		
+
 		// send response
-		if ($r !== false) {
-				
+		if ($r !== false)
+		{
 			$stats = array();
-			foreach ($r as $item) {
+			foreach ($r as $item)
+			{
 				$stats[$item->stat] = $item->val;
 			}
-				
+
 			$response = new Response();
 			$response->setResponseSubject("Estado de la promocion de $source");
 			$response->createFromTemplate('promotor.tpl', array(
@@ -66,10 +67,10 @@ class Estado extends Service
 				'paid' => $stats['PAID'],
 				'source' => $source
 			));
-				
+
 			return $response;
 		}
-		
+
 		$response = new Response();
 		$response->setResponseSubject('No se pudo obtener las estadisticas');
 		$response->createFromText('En estos momentos no se pudo obtener las estad&iacute;sticas que solicitaste;. Por favor, intent&eacute;ntalo m&aacute;s tarde o contacta con el soporte t&eacute;cnico.');
